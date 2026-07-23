4cb03c852f29ade03f64d6673fc0261599fe6b0c
Atchbo <86882671+Atchbo2001@users.noreply.github.com>
2026-07-22 19:33:03 -0500

    Add bot and password server settings

--- pkg/gameserver/config.go
+++ pkg/gameserver/config.go
@@ -1,10 +1,13 @@
 package gameserver
 
 type Config struct {
-	MaxClients       int
-	MatchLength      int
-	DefaultGameSpeed int
-	DefaultMode      string
-	DefaultMap       string
-	Maps             []string
+	MaxClients        int
+	MatchLength       int
+	DefaultGameSpeed  int
+	DefaultMode       string
+	DefaultMap        string
+	Maps              []string
+	PasswordProtected bool
+	BotCount          int
+	BotSkill          int
 }
